017e1e605073d6683b32b0e7698a275abb372e23
cd <x19921111@gmail.com>
2019-11-26 01:02:08 +0800

    npm first build

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h2>{{ msg1 }}</h2>
+    <h1>{{ msg2 }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg1: 'Welcome to My World',
+      msg2: 'Meng-lin Hsieh Protfolio',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
